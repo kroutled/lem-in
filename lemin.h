bae18963d3bb19db926c70406ba301b928ca9cc8
Kyle ROUTLEDGE <kroutled@e4r6p9.wethinkcode.co.za>
2017-10-26 10:29:47 +0200

    structs

--- lemin.h
+++ lemin.h
@@ -3,5 +3,20 @@
 
 #include "./libft/libft.h"
 
+typedef struct	s_room t_room;
+typedef struct	s_anthill t_anthill;
+
+struct s_room
+{
+	char		*name;
+	int			start;
+	int			end;
+	t_room		*roomlinks[100000];
+}
+
+struct	anthill
+{
+	t_room		*rooms[100000];
+}
 
 #endif
