987f187c105396f177553c178d7147ea8798b061
Kyle ROUTLEDGE <kroutled@e4r6p11.wethinkcode.co.za>
2017-11-06 16:11:37 +0200

    fixed segfault

--- main.c
+++ main.c
@@ -6,34 +6,36 @@
 /*   By: kroutled <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/26 17:39:06 by kroutled          #+#    #+#             */
-/*   Updated: 2017/10/26 18:01:55 by kroutled         ###   ########.fr       */
+/*   Updated: 2017/11/06 16:10:41 by kroutled         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lemin.h"
 #include <stdio.h>
-
-int		ft_startend(t_args *args, t_vars *vars)
-{
-	if (ft_strstr(args->data[vars->count], "start"))
-	{
-		ft_putendl("start");
-	}
-	else if(ft_strstr(args->data[vars->count], "end"))
-	{
-		ft_putendl("end");
-	}
-	return (0);
-}
-
+/*
+   int		ft_startend(t_args *args, t_vars *vars)
+   {
+   if (ft_strstr(args->data[vars->count], "start"))
+   {
+   ft_putendl("start");
+   }
+   else if(ft_strstr(args->data[vars->count], "end"))
+   {
+   ft_putendl("end");
+   }
+   return (0);
+   }
+   */
 void	ft_setrooms(t_args *args, t_vars *vars)
 {
 	t_room	*rooms;
-	ft_bzero(&rooms, sizeof(t_room));
+	rooms = (t_room*)ft_memalloc(sizeof(t_room));
+	//	ft_bzero(&rooms, sizeof(t_room));
 	vars->count = 0;
 
 	while (args->data[vars->count])
 	{
+		//ft_putendl(args->data[vars->count]);
 		if (args->data[vars->count][0] == '#')
 		{
 			if (ft_strstr(args->data[vars->count], "start"))
@@ -42,12 +44,24 @@ void	ft_setrooms(t_args *args, t_vars *vars)
 				rooms->start = 1;
 				rooms->name = &args->data[vars->count][0];
 				ft_putnbr(rooms->start);
+				ft_putstr("\n");
 				ft_putendl(rooms->name);
+				ft_putendl("--------------------------");
+			}
+			else if (ft_strstr(args->data[vars->count], "end"))
+			{
+				vars->count++;
+				rooms->end = 1;
+				rooms->name = &args->data[vars->count][0];
+				ft_putnbr(rooms->end);
+				ft_putstr("\n");
+				ft_putendl(rooms->name);
+
 			}
 		}
 		else
 		{
-			vars->count++;
+			//vars->count++;
 		}
 		vars->count++;
 	}	  
@@ -77,11 +91,13 @@ void	ft_anthill(t_args *args, t_vars *vars)
 int main(int ac, char **av)
 {
 	int 	fd;
-	t_args	args;
-	t_vars	vars;
+	t_args	*args;
+	t_vars	*vars;
 
-	ft_bzero(&args, sizeof(t_args));
-	ft_bzero(&vars, sizeof(t_vars));
+	args = (t_args*)ft_memalloc(sizeof(t_args));
+	vars = (t_vars*)ft_memalloc(sizeof(t_vars));
+	//ft_bzero(&args, sizeof(t_args));
+	//ft_bzero(&vars, sizeof(t_vars));
 	if (ac != 2)
 	{
 		ft_putendl("Usage: ./lem-in [file]");
@@ -89,9 +105,9 @@ int main(int ac, char **av)
 	else
 	{
 		fd = open(av[1], O_RDONLY);
-		args.line = ft_readfile(fd);
-		args.data = ft_strsplit(args.line, '\n');
-		ft_anthill(&args, &vars);
+		args->line = ft_readfile(fd);
+		args->data = ft_strsplit(args->line, '\n');
+		ft_anthill(args, vars);
 	}
 	return (0);	
 }
